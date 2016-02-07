0addedbd94556bd32788aabdec47573f5aac5e73
Nicholas Sellek <ngsellek@gmail.com>
2016-02-07 18:58:05 -0500

    Added average rating to individual Restaurant page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207214626) do
+ActiveRecord::Schema.define(version: 20160207230746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20160207214626) do
     t.integer  "price"
     t.string   "food"
     t.string   "address"
-    t.float    "raiting"
     t.text     "description"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
@@ -34,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160207214626) do
     t.integer  "restaurant_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.float    "raiting"
   end
 
 end
