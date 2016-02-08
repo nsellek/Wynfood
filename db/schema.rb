97530b94885da7a13ddb30a998a3bca03a4f2eb2
Alex Melcon <amelc001@fiu.edu>
2016-02-07 19:12:51 -0500

    updated restautant html css

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,6 @@ ActiveRecord::Schema.define(version: 20160206220843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "price"
